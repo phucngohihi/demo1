23395418ac7a9e6ed9026e0f5707c11a0b1e9d57
phucngohihi <phuccpzz12345@gmail.com>
2026-01-23 15:41:12 +0700

    demo2

---
+++ Assets/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float flySpeed; // Biến tốc độ bay [cite: 51]
+
+    void Update()
+    {
+        // Tạo vị trí mới dựa trên vị trí hiện tại
+        var newPosition = transform.position;
+
+        // Tăng tọa độ Y để đạn bay lên trên theo thời gian
+        newPosition.y += Time.deltaTime * flySpeed; // [cite: 55]
+
+        // Gán lại vị trí mới cho viên đạn
+        transform.position = newPosition; // [cite: 56]
+    }
+}
---
+++ Assets/PlayerShootingg.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootingg : MonoBehaviour
+{
+    public GameObject bulletPrefabs;
+    public float shootingInterval;
+    private float lastBulletTime;
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time - lastBulletTime >
+            shootingInterval)
+            {
+                ShootBullet();
+                lastBulletTime = Time.time;
+            }
+        }
+    }
+
+    private void ShootBullet()
+    {
+        Instantiate(bulletPrefabs, transform.position, transform.rotation);
+    }
+}
